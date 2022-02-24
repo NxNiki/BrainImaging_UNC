5588286056b2b5fca6e3d8dd9653f9afb1db6411
NxNiki <michaelniki1988@gmail.com>
2022-02-24 15:36:04 -0500

    read adni_dod_mri

--- script01_dcm2nii.sh
+++ script01_dcm2nii.sh
@@ -2,20 +2,26 @@
 
 ## convert dcm files to nii for rsfMRI data:
 
-data_dir='/ram/USERS/xin/ADNI_FMRI'
+#data_dir='/ram/USERS/xin/ADNI_FMRI'
+data_dir='/ram/USERS/xin/ADNI_DOD_MRI'
 
-out_file='out01_adni_fmri2.txt'
+out_file='out01_adnidod_fmri.txt'
 
 ## list all the subdirectories to identifiy folders for rsfMRI data:
-ls -d $data_dir/*_S_*/*/ | awk -F '/' '{print $6}' | sort | uniq > out01_uniq_dir_adnidod_fmri.txt
-ls -d $data_dir/*_S_*/*/*/*/ | sort > out01_all_dir_adnidod_fmri.txt
+ls -d $data_dir/0*/*/ | awk -F '/' '{print $6}' | sort | uniq > out01_uniq_dir_adnidod_mri.txt
+ls -d $data_dir/0*/*/*/*/ | sort > out01_all_dir_adnidod_mri.txt
 
 ## create a list of directories of rsfMRI dcm files.
 
-#ls -d $data_dir/0*/Axial_rsfMRI*/*/I*>out01_rsfmri_dir1.txt
+ls -d $data_dir/0*/Axial_fcMRI*/*/I*>$out_file
+ls -d $data_dir/0*/*rsFMRI*/*/I*>>$out_file
+ls -d $data_dir/0*/*rsfMRI*/*/I*>>$out_file
+ls -d $data_dir/0*/AXIAL_RS_fMRI*/*/I*>>$out_file
+
 #ls -d $data_dir/0*/*rs[f,F]MRI*/*/I*>$out_file
-ls -d $data_dir/0*/*Resting_State_fMRI*/*/I*>$out_file
-ls -d $data_dir/0*/*resting_state/*/I*>>$out_file
+#ls -d $data_dir/0*/*Resting_State_fMRI*/*/I*>$out_file
+#ls -d $data_dir/0*/*resting_state/*/I*>>$out_file
+
 
 #ls -d $data_dir/*_S_*/*Resting_State_fMRI/*/I*>$out_file
 #ls -d $data_dir/*_S_*/*fcMRI*/*/I*>out01_rsfmri_dir4.txt
