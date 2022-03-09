d73cb0760c3d2d558af8020ce12e74efb814fc92
NxNiki <michaelniki1988@gmail.com>
2022-03-09 15:54:59 -0500

    copy most recent t1 image for each session of fmri

--- script01_match_t1_fmri.sh
+++ script01_match_t1_fmri.sh
@@ -1,32 +1,33 @@
 #!/bin/sh
 
-## list fmri and mri files and find ones with common subjects and put them into a single directory for each subject
+## list fmri and mri files and find ones with common subjects 
 
-## fmri files:
+## fmri and t1 directory:
 
 data_dir_fmri='/home/xin/Downloads/ADNI_FMRI'
 data_dir_t1='/home/xin/Downloads/ADNI_T1'
-
-out_dir="out02_adni_fmri_t1"
-
-if [ ! -d $out_dir ]
-then
-	mkdir $out_dir 
-fi
 
 # for T1 images:
 ls -d $data_dir_t1/*_S_*/MPRAGE/*/S*/*.nii>temp_t1.txt
 
 # rsfMRI images
-ls -d $data_dir/*_S_*/*rs[f,F]MRI*/*/I*/*.nii>temp_fun.txt
-ls -d $data_dir/*_S_*/*fcMRI*/*/I*/*.nii>>temp_fun.txt
-ls -d $data_dir/*_S_*/*Resting_State_fMRI/*/I/*.nii>>temp_fun.txt
-ls -d $data_dir/*_S_*/AXIAL_RS_fMRI*/*/I*/*.nii>>temp_fun.txt
+ls -d $data_dir_fmri/*_S_*/*rs[f,F]MRI*/*/I*>temp_fun.txt
+ls -d $data_dir_fmri/*_S_*/*fcMRI*/*/I*>>temp_fun.txt
+ls -d $data_dir_fmri/*_S_*/*Resting_State_fMRI/*/I*>>temp_fun.txt
+ls -d $data_dir_fmri/*_S_*/AXIAL_RS_fMRI*/*/I*>>temp_fun.txt
 
-ls -d $data_dir/*_S_*/*rs[f,F]MRI*/*/I*/*.json>temp_json.txt
-ls -d $data_dir/*_S_*/*fcMRI*/*/I*/*.json>>temp_json.txt
-ls -d $data_dir/*_S_*/*Resting_State_fMRI/*/I/*.json>>temp_json.txt
-ls -d $data_dir/*_S_*/Axial_fcMRI*/*/I*/*.json>>temp_json.txt
+#ls -d $data_dir_fmri/*_S_*/*rs[f,F]MRI*/*/I*/*.nii>temp_fun.txt
+#ls -d $data_dir_fmri/*_S_*/*fcMRI*/*/I*/*.nii>>temp_fun.txt
+#ls -d $data_dir_fmri/*_S_*/*Resting_State_fMRI/*/I*/*.nii>>temp_fun.txt
+#ls -d $data_dir_fmri/*_S_*/AXIAL_RS_fMRI*/*/I*/*.nii>>temp_fun.txt
 
-cat temp_t1.txt | awk -F '/' '{print $5 $7}' > sub_session_t1.txt
-cat temp_fun.txt | awk -F '/' '{print $5 $7}' > sub_session_fun.txt
+#ls -d $data_dir_fmri/*_S_*/*rs[f,F]MRI*/*/I*/*.json>temp_json.txt
+#ls -d $data_dir_fmri/*_S_*/*fcMRI*/*/I*/*.json>>temp_json.txt
+#ls -d $data_dir_fmri/*_S_*/*Resting_State_fMRI/*/I/*.json>>temp_json.txt
+#ls -d $data_dir_fmri/*_S_*/Axial_fcMRI*/*/I*/*.json>>temp_json.txt
+
+cat temp_t1.txt | awk -F '/' '{print $6}' | sort | uniq > temp_sub_t1.txt
+cat temp_fun.txt | awk -F '/' '{print $6}' | sort | uniq > temp_sub_fun.txt
+comm -12 temp_sub_t1.txt temp_sub_fun.txt > out01_sub_comm.txt
+
+rm temp*.txt
---
+++ script02_copy_data_t1_fmri.sh
@@ -0,0 +1,43 @@
+#!/bin/sh
+
+# copy t1 and fmri images into a single directory for each subject
+
+## fmri files:
+
+data_dir_fmri='/home/xin/Downloads/ADNI_FMRI'
+data_dir_t1='/home/xin/Downloads/ADNI_T1'
+
+out_dir="out02_adni_fmri_t1"
+
+if [ ! -d $out_dir ]
+then
+	mkdir $out_dir 
+fi
+
+while read sub
+do
+    subid='sub-'$(echo $sub) 
+    # list all sessions of fmri data for current subject:
+
+    ls -d $data_dir_fmri/$sub/*rs[f,F]MRI*/*/I*>temp_fun_sub.txt
+    ls -d $data_dir_fmri/$sub/*fcMRI*/*/I*>>temp_fun_sub.txt
+    ls -d $data_dir_fmri/$sub/*Resting_State_fMRI/*/I*>>temp_fun_sub.txt
+    ls -d $data_dir_fmri/$sub/AXIAL_RS_fMRI*/*/I*>>temp_fun_sub.txt
+    echo 'temp_fun_sub.txt:'
+    cat temp_fun_sub.txt
+
+    t1_file=$(ls -d $data_dir_t1/$sub/MPRAGE/*/S*/*.nii | tail -1)
+    echo $t1_file
+
+    while read line2
+    do
+        sesid='ses-'$(echo $line2 | awk -F '/' '{print $8}') 
+
+        mkdir -p $out_dir/$subid/$sesid/anat
+        mkdir -p $out_dir/$subid/$sesid/func
+
+        #cp $line2/*.nii $out_dir/$subid/$sesid/func/
+        cp $t1_file $out_dir/$subid/$sesid/anat/
+
+    done < temp_fun_sub.txt
+done < out01_sub_comm.txt
