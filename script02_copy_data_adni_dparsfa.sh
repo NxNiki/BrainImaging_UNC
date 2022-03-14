dfc491a05eb58e26ad50ee8bc568ed38a454df83
NxNiki <michaelniki1988@gmail.com>
2022-03-14 15:25:46 -0400

    change destine file name for bold and t1 images for cpac

--- script02_copy_data_adni_dparsfa.sh
+++ script02_copy_data_adni_dparsfa.sh
@@ -7,7 +7,7 @@
 data_dir_fmri='/home/xin/Downloads/ADNI_FMRI'
 data_dir_t1='/home/xin/Downloads/ADNI_T1'
 
-out_dir="out02_adni_fmri_t1"
+out_dir="out02_adni_fmri_t1_dpsfa"
 
 if [ ! -d $out_dir ]
 then
@@ -19,32 +19,29 @@ do
     subid='sub-'$(echo $sub) 
     # list all sessions of fmri data for current subject:
 
-    ls -d $data_dir_fmri/$sub/*rs[f,F]MRI*/*/I*>temp_fun_sub.txt
-    ls -d $data_dir_fmri/$sub/*fcMRI*/*/I*>>temp_fun_sub.txt
-    ls -d $data_dir_fmri/$sub/*Resting_State_fMRI/*/I*>>temp_fun_sub.txt
-    ls -d $data_dir_fmri/$sub/AXIAL_RS_fMRI*/*/I*>>temp_fun_sub.txt
+    ls -d $data_dir_fmri/$sub/*rs[f,F]MRI*/*/I*/*.nii>temp_fun_sub.txt
+    ls -d $data_dir_fmri/$sub/*fcMRI*/*/I*/*.nii>>temp_fun_sub.txt
+    ls -d $data_dir_fmri/$sub/*Resting_State_fMRI/*/I*/*.nii>>temp_fun_sub.txt
+    ls -d $data_dir_fmri/$sub/AXIAL_RS_fMRI*/*/I*/*.nii>>temp_fun_sub.txt
     echo 'temp_fun_sub.txt:'
     cat temp_fun_sub.txt
 
     t1_file=$(ls -d $data_dir_t1/$sub/MPRAGE/*/S*/*.nii | tail -1)
-    #t1_session=
+    t1_session=$(echo $t1_file | awk -F '/' '{print $8}')
     echo $t1_file
 
     while read line2
     do
         sesid='ses-'$(echo $line2 | awk -F '/' '{print $8}') 
 
-        mkdir -p $out_dir/$subid/$sesid/anat
-        mkdir -p $out_dir/$subid/$sesid/func
+        mkdir -p $out_dir/FunImg/${subid}-${sesid}/
+        mkdir -p $out_dir/T1Img/${subid}-${sesid}
 
-        cp $line2/*.nii $out_dir/$subid/$sesid/func/rest_bold.nii
-        cp $line2/*.json $out_dir/$subid/$sesid/func/rest_bold.json
-        gzip $out_dir/$subid/$sesid/func/rest_bold.nii
+        cp -l $line2 $out_dir/FunImg/${subid}-${sesid}/rest_bold.nii
 
         ## NOTE: we modify the session id of T1 images to match that of fmri images here.
         ## use t1_session to keep the original info on session ID for T1 images.
-        cp $t1_file $out_dir/$subid/$sesid/anat/T1w.nii
-        gzip $out_dir/$subid/$sesid/anat/T1w.nii
+        cp -l $t1_file $out_dir/T1Img/${subid}-${sesid}/T1w.nii
 
     done < temp_fun_sub.txt
 done < out01_sub_comm.txt
