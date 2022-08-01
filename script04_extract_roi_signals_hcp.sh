9a111eb2e52e650da38e6e150d8db8639d560791
NxNiki <michaelniki1988@gmail.com>
2022-08-01 11:16:42 -0400

    udpate atlas to extract singals

--- script04_extract_roi_signals_hcp.sh
+++ script04_extract_roi_signals_hcp.sh
@@ -23,7 +23,11 @@ out_dir="out04_adni_roi_signals5"
 #out_dir="out04_adni_roi_signals6"
 
 
+#atlas="/home/xin/Downloads/DPABI_V6.1_220101/Templates/Power_Neuron_264ROIs_Radius5_Mask.nii"
+#atlas_name="power264"
+
 atlas="/home/xin/Downloads/Atlases/MNI_Glasser_HCP_2019_v1.0/MNI_Glasser_HCP_v1.0.nii.gz"
+atlas_name="glasser_hcp"
 
 # ---------------------------------------------------
 
@@ -39,10 +43,9 @@ do
     echo $img
     #flirt -in $atlas -ref $img -nosearch -searchrx 0 0 -searchry 0 0 -searchrz 0 0 -interp nearestneighbour -out $out_dir/tmp_atlas.nii
     flirt -in $atlas -ref $mask -nosearch -searchrx 0 0 -searchry 0 0 -searchrz 0 0 -interp nearestneighbour -out $out_dir/tmp_atlas.nii
-    
     # compute mean for each region in the atlas:
     index=1
-    while [ $index -ne 181 ]
+    while [ $index -ne 265 ]
     do
         #echo $index
         # create temp_mask for roi:
@@ -53,28 +56,17 @@ do
         index=$(($index+1))
     done
     
-    index=1000
-    while [ $index -ne 1181 ]
-    do
-        #echo $index
-        # create temp_mask for roi:
-    	fslmaths $out_dir/tmp_atlas -uthr $index -thr $index $out_dir/tmp_mask
-        fslmeants -i $img -o $out_dir/tmp_roi_${index}.txt -m $out_dir/tmp_mask
-    	#fslmaths $img -mul tmp_mask tmp_roi.nii.gz
-    	#fslstats -t tmp_roi.nii.gz -M > tmp_roi_${index}.txt
-        index=$(($index+1))
-    done
 
     ########## Step02: combine data for all atlases ##########
 
     # fill roi index as file header:
-    ls $out_dir/tmp_roi_*.txt | sed 's/^.*\(tmp_roi_\)//'| sed 's/.txt//g' | tr "\n" "\t"> $out_dir/roi_signals_hcp_${sub_id}.txt
+    ls $out_dir/tmp_roi_*.txt | sed 's/^.*\(tmp_roi_\)//'| sed 's/.txt//g' | tr "\n" "\t"> $out_dir/roi_signals_power264_${sub_id}.txt
     
     # add Feed Line, echo automaticlly add \n at the end of line, so we just add an empty charactor:
-    echo "" >> $out_dir/roi_signals_hcp_${sub_id}.txt
+    echo "" >> $out_dir/roi_signals_power264_${sub_id}.txt
     
     # fill roi signals:
-    paste -d "\t" $out_dir/tmp_roi_*.txt >> $out_dir/roi_signals_hcp_${sub_id}.txt
+    paste -d "\t" $out_dir/tmp_roi_*.txt >> $out_dir/roi_signals_power264_${sub_id}.txt
     echo $(cat $out_dir/roi_signals_power264_${sub_id}.txt | wc -l)
     rm $out_dir/tmp_roi_*.txt
     rm $out_dir/tmp_*.nii.gz
